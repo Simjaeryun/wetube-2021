642676000f4302337edada09dc2f60bfce4c9a7f
simjaeryun <simjaeding@gmail.com>
2021-08-27 18:10:31 +0900

    recorder fix

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@ app.use(localsMiddleware);
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use(
