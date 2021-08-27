f626c7820ce5e6d821a19e7c089c83a557087561
simjaeryun <simjaeding@gmail.com>
2021-08-27 19:18:13 +0900

    cors

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(localsMiddleware);
 
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.header("Cross-Origin-Resource-Policy", "same-site");
   next();
 });
